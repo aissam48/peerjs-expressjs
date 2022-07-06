36e8a4d3ed2a9f22a1e84dfb1bbc18294d4db378
PC <tamir@ajicreative.fr>
2022-07-06 20:08:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+// create express peer server
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+var options = {
+    debug: true
+}
+
+// create a http server instance to listen to request
+var server = require('http').createServer(app);
+
+app.get("/", (req,res)=>{
+    res.send("ujgdxskhjll")
+})
+
+// peerjs is the path that the peerjs server will be connected to.
+app.use('/peerjs', ExpressPeerServer(server, options));
+// Now listen to your ip and port.
+server.listen(9000);
